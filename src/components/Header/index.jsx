c8933fb9599fc6c27a9650bf71636df0fa6db158
akankshafaujdar1311 <akankshafaujdar3@gmail.com>
2023-07-27 15:00:51 +0530

    *

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,12 @@
 import { Toolbar, AppBar, Typography } from "@mui/material";
 import Logo from "../Logo";
-
+import {Link} from "react-router-dom";
 const Header = (props) => {
   return (
     <AppBar position="static" style={{ backgroundColor: "#6c88c8" }}>
       <Toolbar>
         <Typography style={{marginRight:"300px",marginLeft:"100px"}}>
-          <Logo />
+          <Link to="/"><Logo /></Link>
         </Typography>
         <Typography color="white" fontSize="30px" style={{marginRight:"300px"}}>
           Dream School Foundation
