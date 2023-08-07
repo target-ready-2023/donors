a99394bc6a593f5597d250836779678041bd905e
akankshafaujdar1311 <akankshafaujdar3@gmail.com>
2023-08-07 13:54:10 +0530

    form submission triggers addDonorInfo and shows success message

--- frontend/src/components/NewCustomer/index.test.jsx
+++ frontend/src/components/NewCustomer/index.test.jsx
@@ -1,52 +1,76 @@
-import axios from 'axios';
-import { addDonorInfo } from '../../services/ApiService';
+import axios from "axios";
+import { addDonorInfo } from "../../services/ApiService";
 import { render, screen, fireEvent } from "@testing-library/react";
 import NewCustomer from "./index";
-import { waitFor } from '@testing-library/react';
-jest.mock('axios');
+import { waitFor } from "@testing-library/react";
 
-describe('New Donor', () => {
+jest.mock("axios");
 
-  const donorData = {donorName : "Akankshaaa",
-    donorAddress : "Jaipur",
+describe("New Donor", () => {
+  const donorData = {
+    donorName: "Akankshaaa",
+    donorAddress: "Jaipur",
     donorEmail: "akankshafaujdar123@gmail.com",
-    dateOfBirth : "2000-12-11",
-    donorPan : "AHQF7122D",
-    transactions : [{
-      amount : '20000',
-      transactionMode : 'UPI',
-      transactionDate : '20-12-2019'
-    }] };
+    dateOfBirth: "2000-12-11",
+    donorPan: "AHQF7122D",
+    transactions: [
+      {
+        amount: "20000",
+        transactionMode: "UPI",
+        transactionDate: "20-12-2019",
+      },
+    ],
+  };
 
   afterEach(() => {
     jest.clearAllMocks();
   });
-//Testing of addDonorInfo 
-  it('should add a new donor', async () => {
-    const response = {status : 201 ,data:{ donorID : 1}};
+
+  //Testing of addDonorInfo
+
+  it("should add a new donor", async () => {
+    const response = { status: 201, data: { donorID: 1 } };
     axios.post.mockResolvedValue(response);
 
     const result = await addDonorInfo(donorData);
 
     expect(axios.post).toHaveBeenCalledTimes(1);
-    expect(axios.post).toHaveBeenCalledWith( `http://localhost:8080/api/donor/addDonor`,donorData);
+    expect(axios.post).toHaveBeenCalledWith(
+      `http://localhost:8080/api/donor/addDonor`,
+      donorData
+    );
     expect(result.data).toEqual(response.data); // Compare only the "data" property of the response
     expect(result.status).toEqual(response.status); // Compare only the "status" property of the response
- 
   });
-//Error Handling
-  it('should handle errors', async () => {
-    const errorResponse = { response: { status: 500, data: { error: 'Server Error' } } };
+  //Error Handling
+  it("should handle errors", async () => {
+    const errorResponse = {
+      response: { status: 500, data: { error: "Server Error" } },
+    };
     axios.post.mockRejectedValue(errorResponse);
 
     await expect(addDonorInfo(donorData)).rejects.toEqual(errorResponse);
   });
-//Rendering NewDonor
+  //Rendering NewDonor
   test("renders the NewDonor component", () => {
     render(NewCustomer);
     const nameInput = screen.queryByLabelText("Name");
-  waitFor(()=>expect(nameInput).toBeInTheDocument());
+    waitFor(() => expect(nameInput).toBeInTheDocument());
   });
 
-  
+  test("form submission triggers addDonorInfo and shows success message",  () => {
+    render(NewCustomer);
+
+    // Fill out the form fields
+   waitFor(()=> fireEvent.change(screen.queryByLabelText("Name"), { target: { value: "John Doe" } }));
+   waitFor(()=> fireEvent.change(screen.queryByLabelText("Address"), { target: { value: "123 Main St" } }));
+    // Fill out other fields...
+
+    // Submit the form
+   waitFor(()=> fireEvent.click(screen.queryByText("Submit")));
+
+    // Wait for the promise to resolve
+    waitFor(() => expect(screen.getByText("You have successfully added !")).toBeInTheDocument());
+  });
+
 });
